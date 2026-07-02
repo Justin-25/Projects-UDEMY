5772f276b07b738d9c3b58b53f9efb6a4644b04f
JustinDev <justinmandia25@gmail.com>
2026-07-02 22:22:30 +0800

    Fix the UI/UX for the Travel tracker and error handling.

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -18,13 +18,17 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+async function visitedCountries() {
+  let countries = []
+  const result = await db.query("SELECT country_code FROM visited_countries");
+    countries = result.rows.map(row => row.country_code)
+
+  return countries;
+}
+
 app.get('/', async (req, res) => {
   //Write your code here.
-  let countryCode = []
-  const result = await db.query("SELECT country_code FROM visited_countries");
-  const rows = result.rows.map(row => row.country_code)
-    countryCode = rows;
-  console.log(countryCode)
+  const countryCode = await visitedCountries(); 
   res.render("index.ejs", {
     countries: countryCode,
     total: countryCode.length
@@ -35,22 +39,35 @@ app.get('/', async (req, res) => {
 
 app.post('/add', async (req, res) => {
   let country = req.body.country;
-  const dbCountries = await db.query("SELECT * FROM countries");
-  const countriesData = dbCountries.rows.find((row) => 
-    row.country_name.toLowerCase() === country.toLowerCase() || row.country_code.toLowerCase() === country.toLowerCase()
-  );
-
-  if (!countriesData){
-    res.send('<div>This country doesnt exist, please try again</div>')
-  } else {
-    await db.query(
-      "INSERT INTO visited_countries (country_code) VALUES ($1)",
-      [countriesData.country_code]
-    );
-    res.redirect('/')
-  }
+  const result = await db.query("SELECT (country_name), (country_code) FROM countries WHERE country_name ILIKE $1", [`%${country}%`]);
+    try {
+      if(result.rows.length !== 0) {
+        const data = result.rows[0];
+        const countryCode = data.country_code
+      
+        await db.query(
+          "INSERT INTO visited_countries (country_code) VALUES ($1)",
+          [countryCode]
+        );
+        res.redirect('/')
+      } else {
+        const countryCode = await visitedCountries();
+        res.render("index.ejs", {
+          countries: countryCode,
+          total: countryCode.length,
+          error: 'Countries does not exist, Please try again...'
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      const countryCode = await visitedCountries();
+      res.render("index.ejs", {
+        countries: countryCode,
+        total: countryCode.length,
+        error: 'Countries already exist, Insert new countries...'
+      })
+    }
 })
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);

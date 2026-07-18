2103edb99b67ae75bed405915e924d0981fb5874
JustinDev <justinmandia25@gmail.com>
2026-07-18 15:51:31 +0800

    Learn: Complex State and Spread Operator

---
+++ 313-changing-complex-state/src/components/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+function App() {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [submit, setSubmit] = useState("")
+
+  // function handleSubmit(event) {
+  //   setSubmit(firstName + " " + lastName)
+
+  //   event.preventDefault()
+  // }
+
+  // function handleFirstName(event) {
+  //   setFirstName(event.target.value)
+  // }
+
+  // function handleLastName(event) {
+  //   setLastName(event.target.value)
+  // }
+
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
